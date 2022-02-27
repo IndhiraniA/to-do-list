ba1145777bb32e2ebb52b09cc5d74e8841df5535
Indhirani <85779631+IndhiraniA@users.noreply.github.com>
2022-02-27 13:02:28 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,12 @@
 // Create a "close" button and append it to each list item
-var mylist = document.getElementsByTagName("LI");
+var mylist = document.getElementsByTagName("LI");  //getElementsByTagName()method is used to tag the element in the list
 var i;
 for (i = 0; i < mylist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7"); //u00D7 is a "x" multiplication symbol
   span.className = "close";
-  span.appendChild(txt);
+  span.appendChild(txt);                     //using appendchild()method append the mul symbol in the list.The appendChild() method allows you to add a node
+                                             //to the end of the list of child to parent.
   mylist[i].appendChild(span);
 }
 
